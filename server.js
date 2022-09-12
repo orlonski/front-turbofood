c97cf8b260e9e0f9f863723c66030c2183f6986a
diego.orlonski <orlonski@icloud.com>
2022-09-11 21:58:19 -0300

    alterações server

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
-const express = require('express')
-const { resolve } = require('path')
+const express = require('express');
+const path = require('path');
 
-const app = express()
+const app = express();
 
-app.use('*',
-    express.static(
-        resolve(
-            __dirname,
-            './build'
-        )
-    )
-)
+app.use(express.static(path.join(__dirname, './build')));
 
 app.listen(process.env.PORT || 3000, (err) => {
     if (err) {
